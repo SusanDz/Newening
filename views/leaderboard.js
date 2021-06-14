60d34110361afe2feef9de2ba463107560e03c7e
sd2009 <sd2009@hw.ac.uk>
2021-06-14 13:57:07 +0400

    add another row if user is not in the top 5 leaderboard

--- views/leaderboard.js
+++ views/leaderboard.js
@@ -1,30 +1,51 @@
 function init() {
     $(document).ready(function() {
-        // if (getCookie("score") != "") {
-        //     var username = getCookie("username");
-        //     var score = getCookie("score");
-        //     setCookie("score", "", 30);
-        //     var userScore = { username: username, score: score };
-        //     $.post('/api/leaderboardUpdate', userScore, function(resultUpd) {})
-        // }
+        var num = null;
+        var player = sessionStorage.getItem("Player");
+        // const elem = document.querySelector(".you");
+        // elem.style.display = "none";
+
         setTimeout(function() {
             $.post('/api/leaderboard', function(result) {
-                    // console.log(result[0]);
-                    var i = 0;
-                    while (i < result.length) {
-                        $("#" + i + "user").html(result[i].name);
-                        $("#" + i + "score").html(result[i].score);
-                        i++;
+                console.log("Hi ", player);
+
+                var i = 0;
+                    
+                while (i < result.length) {
+                    $("#" + i + "user").html(result[i].name);
+                    $("#" + i + "score").html(result[i].score);
+
+                    if(result[i].name == player) {
+                        num=i;
                     }
-                    console.log("test" + result.length);
-                })
-                .fail(function() {
-                    window.alert("test1");
-                    console.log("error loading user");
-                });
+                    i++;
+
+                }
+                console.log("Num",num);
+                $("#" + num + "user").css("border-colour", "yellow");
+
+            })
+            .fail(function() {
+                window.alert("test1");
+                console.log("error loading user");
+            });
 
         }, 2)
 
+        if(num == null){
+            var s= 0;
+            var play = {username:player, score:s};
+            var i=0;
+
+            $.post('/api/getScore', play, function(result) {
+                // elem.style.display = "block";
+                console.log(result);
+                console.log(result[0].id);
+                $("#no").html("6.");
+                $("#nouser").html(result[0].name);
+                $("#noscore").html(result[0].score);
+            })
+        }
     })
 
 };
