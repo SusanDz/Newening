957bfca5b72c6f7f04d3948d484913cac491a181
sd2009 <sd2009@hw.ac.uk>
2021-06-15 20:31:19 +0400

    if user is not a top 5 player make another row and insert the user score. Also highlight user score

--- views/leaderboard.js
+++ views/leaderboard.js
@@ -2,28 +2,35 @@ function init() {
     $(document).ready(function() {
         var num = null;
         var player = sessionStorage.getItem("Player");
-        // const elem = document.querySelector(".you");
-        // elem.style.display = "none";
+        const elemno = document.querySelector("td#no");
+        const elemuser = document.querySelector("td#nouser");
+        const elemscore = document.querySelector("td#noscore");
 
         setTimeout(function() {
             $.post('/api/leaderboard', function(result) {
                 console.log("Hi ", player);
 
                 var i = 0;
-                    
-                while (i < result.length) {
+
+                while (i < 5) {
                     $("#" + i + "user").html(result[i].name);
                     $("#" + i + "score").html(result[i].score);
 
                     if(result[i].name == player) {
                         num=i;
+                        sessionStorage.setItem("num", i);
+                        elemno.style.display = "none";
+                        elemuser.style.display = "none";
+                        elemscore.style.display = "none";
                     }
                     i++;
 
                 }
-                console.log("Num",num);
-                $("#" + num + "user").css("border-colour", "yellow");
-
+                if(num != null) {
+                    document.getElementById(num).bgColor = 'orange';
+                } else {
+                    document.getElementById('5').bgColor = 'orange';
+                }
             })
             .fail(function() {
                 window.alert("test1");
@@ -32,18 +39,18 @@ function init() {
 
         }, 2)
 
-        if(num == null){
-            var s= 0;
-            var play = {username:player, score:s};
+        if(num === null && player!=null){
             var i=0;
 
-            $.post('/api/getScore', play, function(result) {
-                // elem.style.display = "block";
-                console.log(result);
-                console.log(result[0].id);
-                $("#no").html("6.");
-                $("#nouser").html(result[0].name);
-                $("#noscore").html(result[0].score);
+            $.post('/api/leaderboard', function(result) {
+                
+                while(result[i].name != player) {
+                    i++;
+                }
+
+                $("#no").html(i+1 +".");
+                $("#nouser").html(result[i].name);
+                $("#noscore").html(result[i].score);
             })
         }
     })
